6f5e65691aabd0b479ebb92b5c607096aa2f1ec9
jkobitos <j4285603@gmail.com>
2017-08-09 23:47:51 +0800

    homework initialized

---
+++ MoneyManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoneyManager.Startup))]
+namespace MoneyManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
